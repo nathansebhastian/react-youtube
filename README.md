38ac60252f800f11b3447e5f5f322bac424a18e5
Yonathan <yonathansebhastian@gmail.com>
2016-11-21 17:13:58 +0700

    search state OK

--- README.md
+++ README.md
@@ -11,9 +11,13 @@ Clone this repo, install dependencies, then start the process with the following
 	> npm start
 	> Open browser at localhost:8080
 ```
+
 ####What I Learned from this project####
 * Using React components
 * How to troubleshoot code errors
 * What webpack and babel are used for
 * Using JSX
 * YouTube video api
+* Class-based component
+* Handling user event
+* State in React
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBQzgSB0LbfFHmfD9uSq-bfWUfYw4L7bFY';
 
 // Create a new component class. This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (component instance, target)
