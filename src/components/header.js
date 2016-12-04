65bc0dee7fe83b8385f67710dfc5717d215aae4e
yonathansebhastian <yonathansebhastian@gmail.com>
2016-12-04 12:16:19 +0700

    header

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MyHeader = () => {
   return (
-    <div class="jumbotron">
+    <div className="jumbotron">
       <h1>React Youtube Video Player</h1>
       <p>Learning the basics of using React, the most popular front-end framework that is taking the world by storm. <br />You can search and play any youtube video you want here. <br />Note: This API search term will default to <strong>SingSing</strong>&#44; a dota2 player famous for his hippie play style and funny comments.</p>
     </div>
